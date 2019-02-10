39f9c3fe9cd54c7bc38a91f0cf898de5c37c427f
mado-is-here <amado.m728@gmail.com>
2019-02-10 15:30:47 -0500

    added theme data module and state blueprint for mood selector and app components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react';
-import Navbar from './components/Navbar';
-import ColorBox from './components/ColorBox';
-import Footer from './components/Footer';
-import MoodSelector from './components/MoodSelector';
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import ColorBox from "./components/ColorBox";
+import Footer from "./components/Footer";
+import MoodSelector from "./components/MoodSelector";
+
+const initialState = {
+  theme: "summer", // a theme is a pre-chosen mood and music combination
+  mood: "auto", // this can be custom chosen, or set to auto which follows theme
+  music: "auto", // this can be custom chosen, or set to auto which follows theme
+  fullscreen: false
+};
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = initialState;
+  }
 
-   render() {
-      return (
-	      <div>
-	        <Navbar />
-	        <MoodSelector />
-	        <ColorBox />
-	        <Footer />
-	      </div>
-      );
-   }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <MoodSelector />
+        <ColorBox />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/modules/theme-data.js
@@ -0,0 +1,22 @@
+const categories = [
+  {
+    name: "Emotions",
+    moods: ["happiness", "love", "sadness"]
+  },
+  {
+    name: "Holidays",
+    moods: ["christmas", "halloween"]
+  },
+  {
+    name: "Seasons",
+    moods: ["spring", "summer", "autumn", "winter"]
+  },
+  {
+    name: "Vibes",
+    moods: ["city", "gamer", "sunset", "rainbow"]
+  }
+];
+
+module.exports = {
+  categories
+};
